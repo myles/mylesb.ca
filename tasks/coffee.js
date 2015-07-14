3c31726299d29dc8f441e804832bbd34573c19a9
Myles Braithwaite <me@mylesbraithwaite.com>
2015-07-13 20:42:19 -0400

    Did some stuff.

--- tasks/coffee.js
+++ tasks/coffee.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
     var config = {
         compile: {
+            options: {
+                sourceMap: true
+            },
             files: {
                 'build/assets/javascript/application.js': [
                     'source/assets/javascript/application.coffee'
--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,9 +1,19 @@
 module.exports = function (grunt) {
     var config = {
         dist: {
+            options: {
+                sourceMap: true
+            },
             files: {
-                'build/assets/javascript/application.js': ['build/assets/javascript/application.js'],
-                'build/assets/javascript/vendor.js': ['build/assets/javascript/vendor.js']
+                'build/assets/javascript/vendor.js': [
+                    'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/imager.js/dist/Imager.min.js',
+                    'bower_components/concise/dist/js/concise.min.js',
+                    'bower_components/svg-injector/dist/svg-injector.min.js'
+                ],
+                'build/assets/javascript/application.js': [
+                    'build/assets/javascript/application.js'
+                ]
             }
         }
     };
