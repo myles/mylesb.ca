2d9d39dbb420d91e11fac40a776dbc6452e2a1b4
Myles Braithwaite <me@mylesbraithwaite.com>
2015-12-03 18:10:51 -0500

    Playing around with changing backgrounds and popovers.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-htmllint');
     grunt.loadNpmTasks('grunt-scss-lint');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
