465f118b9558c9d360a033d5af1fe043bca66d2c
Myles Braithwaite <me@mylesbraithwaite.com>
2016-01-23 17:59:53 -0500

    Adding some of my OTR fingerprints.

--- tasks/exec.js
+++ tasks/exec.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
 
     var config = {
         public_key: 'gpg --armor --export <%= config.gpg_short_id %> > <%= config.uploads %>/<%= config.gpg_short_id %>.asc',
+        xmpp_otr_fingerprints: 'gpg -u <%=config.gpg_short_id %> --clearsign <%= config.uploads %>/xmpp-otr-fingerprints.txt',
         proof_of_identity: 'gpg -u <%= config.gpg_short_id %> --clearsign  <%= config.uploads %>/proof-of-identity.txt',
         obsolete_public_key: 'gpg --armor --export <%= config.obsolete_gpg_short_id %> > <%= config.uploads %>/<%= config.obsolete_gpg_short_id %>.asc',
         obsolete_proof: 'gpg -u <%= config.obsolete_gpg_short_id %> --clearsign <%= config.uploads %>/obsolete-<%= config.obsolete_gpg_short_id %>-proof.txt',
