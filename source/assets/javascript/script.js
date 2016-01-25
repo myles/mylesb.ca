01729aff01a2b755dc312759e7a3ff9865c6eaea
Myles Braithwaite <me@mylesbraithwaite.com>
2016-01-25 10:29:36 -0500

    Making sure fingerprints are legiable.

---
+++ source/assets/javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var regexNumber = /(\d+)/g;
+    
+    $('.fingerprint').each(function(index) {
+        var codeFingerprintText = $(this).text();
+        $(this).html(
+            codeFingerprintText.replace(
+                regexNumber,
+                '<span class="number">$&</span>'
+            )
+        );
+    });
+});
