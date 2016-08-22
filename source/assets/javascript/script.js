359d8d9563dfcd135710b6dd3c91c97d7efd0851
Myles Braithwaite <me@mylesbraithwaite.com>
2016-08-22 16:15:54 -0400

    Did a lot of work refactoring some stuff.

--- source/assets/javascript/script.js
+++ source/assets/javascript/script.js
@@ -17,6 +17,19 @@ function trackOutboundLinkClicks(link) {
 $(document).ready(function() {
     'use strict';
 
+    var menuToggle = $('#js-navigation-mobile-menu').unbind();
+    $("#js-navigation-menu").removeClass("navigation__list--show");
+
+    menuToggle.on('click', function (e) {
+        e.preventDefault();
+
+        $("#js-navigation-menu").slideToggle(function () {
+            if ($("#js-navigation-menu").is(":hidden")) {
+                $("#js-navigation-menu").removeAttr("style");
+            }
+        });
+    });
+
     $('a:not([href*="' + document.domain + '"])').mousedown(function (event) {
         // Just in case, be safe and don't do anything
         if (ga === undefined) {
@@ -28,7 +41,7 @@ $(document).ready(function() {
     });
 
     var regexNumber = /(\d+)/g;
-    
+
     $('.fingerprint').each(function(index) {
         var codeFingerprintText = $(this).text();
         $(this).html(
--- tasks/uglify.js
+++ tasks/uglify.js
@@ -5,7 +5,6 @@ module.exports = function (grunt) {
 
     files = {
         '<%= config.destination %>/assets/javascript/script.js': [
-            '<%= config.assets %>/javascript/svg-injector.js',
             '<%= config.assets %>/javascript/script.js'
         ]
     };
