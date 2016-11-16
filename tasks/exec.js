a78f72d77cf97ecd7654afb11ae4d299b8db3f54
Myles Braithwaite <me@mylesbraithwaite.com>
2016-11-16 10:32:58 -0500

    Added more information about my different gpg keys.

--- tasks/exec.js
+++ tasks/exec.js
@@ -11,9 +11,10 @@ module.exports = function (grunt) {
         ssh_key_fingerprints: 'gpg -u <%=config.gpg_short_id %> --clearsign <%= config.uploads %>/ssh-key-fingerprints.txt',
 
         work_public_key: 'gpg --armor --export <%= config.work_gpg_short_id %> > <%= config.uploads %>/<%= config.work_gpg_short_id %>.asc',
-        work_proof_of_identity: 'gpg -u <%= config.work_gpg_short_id %> -u <%= config.gpg_short_id %> --clearsign  <%= config.uploads %>/proof-of-identity-work.txt',
+        work_proof_of_identity: 'gpg -u <%= config.work_gpg_short_id %> -u <%= config.gpg_short_id %> -u <%= config.bgi_gpg_short_id %> --clearsign <%= config.uploads %>/proof-of-identity-work.txt',
 
         bgi_public_key: 'gpg --armor --export <%= config.bgi_gpg_short_id %> > <%= config.uploads %>/<%= config.bgi_gpg_short_id %>.asc',
+        bgi_proof_of_identity: 'gpg -u <%= config.bgi_gpg_short_id %> -u <%= config.gpg_short_id %> -u <%= config.work_gpg_short_id %> --clearsign <%= config.uploads %>/proof-of-identity-braithwaite-network.txt',
 
         obsolete_public_key: 'gpg --armor --export <%= config.obsolete_gpg_short_id %> > <%= config.uploads %>/<%= config.obsolete_gpg_short_id %>.asc',
         obsolete_proof: 'gpg --clearsign -u <%= config.obsolete_gpg_short_id %> -u <%= config.gpg_short_id %> <%= config.uploads %>/obsolete-<%= config.obsolete_gpg_short_id %>-proof.txt'
