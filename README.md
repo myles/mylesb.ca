4bddb66430018e6582fe4fa627df8cc3ed124826
Myles Braithwaite <me@mylesbraithwaite.com>
2019-07-27 20:03:04 -0400

    :tada: Started working on 2019 redesign.

---
+++ README.md
@@ -0,0 +1,6 @@
+   # :ghost: [MylesB.ca](https://mylesb.ca/)
+   
+[![Netlify Status](https://api.netlify.com/api/v1/badges/cc609ea6-27a4-4844-86cb-c696bdd7b0f9/deploy-status)](https://app.netlify.com/sites/myles/deploys)
+[![GitHub Project](https://img.shields.io/badge/GitHub-Project-brightgreen.svg)](https://github.com/users/myles/projects/4)
+   
+ Hi, this is the source code for [my homepage](https://mylesb.ca/ "Myles Braithwaite").
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: 'file-loader?name=[name].[ext]',
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: false,
+              collapseWhitespace: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'postcss-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          },
+        ],
+      },
+    ],
+  },
+};
